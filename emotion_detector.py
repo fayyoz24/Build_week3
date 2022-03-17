d483cd5b137b4da31d7ac4cb71fae1fabe3be987
Fayyoz24 <fayyozusmon@gmail.com>
2022-03-17 15:57:27 +0100

    Update emotion_detector.py

--- emotion_detector.py
+++ emotion_detector.py
@@ -1,6 +1,17 @@
+from ast import arg
 from torchvision.transforms import ToPILImage, Grayscale, ToTensor, Resize
 from torchvision import transforms
 import torch.nn.functional as F
 import numpy as np
 import cv2
-import model
+from model import Emotion
+import torch 
+import argparse
+
+
+# cascade = 'https://github.com/akmadan/Emotion_Detection_CNN/blob/main/haarcascade_frontalface_default.xml'
+# print(cascade)
+parser = argparse.ArgumentParser()
+model = Emotion(num_of_channels=1, num_of_classes=5)
+model.load_state_dict(torch.load('./model.pth'))
+# model.eval()
